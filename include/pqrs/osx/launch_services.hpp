4be2302d378dc17048666fb4effc2fe7ae4df671
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-24 13:42:22 +0900

    Version 1.0.0

--- include/pqrs/osx/launch_services.hpp
+++ include/pqrs/osx/launch_services.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::launch_services v0.0
+// pqrs::osx::launch_services v1.0
 
 // (C) Copyright Takayama Fumihiko 2022.
 // Distributed under the Boost Software License, Version 1.0.
