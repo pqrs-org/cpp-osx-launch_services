8ad5712bfc86decdc8788d8835f92f8275b1a479
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-05 19:04:02 +0900

    Add update argument into register_application

--- example/main.cpp
+++ example/main.cpp
@@ -3,6 +3,7 @@
 
 int main(void) {
   std::cout << "TextEdit: " << pqrs::osx::launch_services::register_application("/System/Applications/TextEdit.app") << std::endl;
+  std::cout << "TextEdit (no update): " << pqrs::osx::launch_services::register_application("/System/Applications/TextEdit.app", false) << std::endl;
   std::cout << "NotFound: " << pqrs::osx::launch_services::register_application("/Applications/NotFound.app") << std::endl;
 
   return 0;
