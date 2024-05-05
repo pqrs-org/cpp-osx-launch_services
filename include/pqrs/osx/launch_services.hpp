2ca5d36c7b5113978d4246ebe76ff916a6c48a48
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-05 19:04:39 +0900

    http -> https

--- include/pqrs/osx/launch_services.hpp
+++ include/pqrs/osx/launch_services.hpp
@@ -4,7 +4,7 @@
 
 // (C) Copyright Takayama Fumihiko 2022.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "launch_services/status.hpp"
 #include <pqrs/cf/string.hpp>
